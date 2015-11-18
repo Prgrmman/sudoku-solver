83621ffe7c2f2fb77d9f10b9b238346aaae99cb9
Jon <jterner1@binghamton.edu>
2015-11-18 16:05:05 -0500

    Finished tester class to work with Solver class

--- JavaCore/Tester.java
+++ JavaCore/Tester.java
@@ -5,14 +5,27 @@ public class Tester{
     public static void main(String[] args){
 	System.out.println("Hello");
 	Scanner in = new Scanner (System.in);
-	int[] nums = new int[81];
+	byte[] nums = new byte[81];
 	int i = 0;
 	
 	while(in.hasNextInt() && i < 81)
-	    nums[i++] = in.nextInt();
+	    nums[i++] = (byte)in.nextInt();
 
-       
+	// a test of our doubly linked list class
+	DLinkedList<Integer> list = new DLinkedList(new Integer(5));
+       	list.append(new Integer(4));
+	
+	Solver solver = new Solver(nums);
+	System.out.println("Printing our puzzle");
+	System.out.println(solver.toString());
+
+	System.out.println("Is there a solution: " +solver.solve());
+	System.out.println(solver.toString());
+	System.out.println("End of program");
+	
+    
     }
+    
 
  
 
